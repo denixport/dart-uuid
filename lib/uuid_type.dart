6f863e2cb15d29576e7f5946a4cc840005b1c57b
denixport <denixport@gmail.com>
2019-03-06 07:00:50 +0300

    fix doc headers

--- lib/uuid_type.dart
+++ lib/uuid_type.dart
@@ -1,11 +1,11 @@
-// Copyright (c) 2018, Denis Portnov. All rights reserved.
+// Copyright (c) 2018-2019, Denis Portnov. All rights reserved.
 // Released under MIT License that can be found in the LICENSE file.
 
 library uuid_type;
 
-/// This library provides UUID type for Dart. It allows to read UUID from string
-/// and binary representations, use it as [Map] key, compare and sort UUIDs
-/// and generate various types of UUID as defined in
+/// This library provides UUID type for Dart. It allows you to read UUID from
+/// string and binary representations, use it as `Map` key,
+/// compare and sort UUIDs and generate various types of UUID as defined in
 /// [RFC 4122](https://tools.ietf.org/html/rfc4122)
 
 export 'src/generators.dart';
